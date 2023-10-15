cf2a482c87ba873bb22b095c2b5f2376f823bb89
WAYKE4 <gitta.tacrhi@mail.ru>
2023-10-15 23:59:13 +0300

    second commit (master)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello"); //first commit
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt(); // second commit
+
     }
 }
